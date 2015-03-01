768e942f487feb160d50e23c2de1cae77661c53f
rodrigodemingo <rodrigodemingo@gmail.com>
2015-03-01 12:45:12 +0400

    Update

--- wp-content/themes/parkcollege/functions/shortcodes/dropcap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function dropcap_shortcode( $atts, $content = null ) {
-	return '<span class="dropcap">' . do_shortcode($content) . '</span>';
-}
-
-add_shortcode( 'dropcap', 'dropcap_shortcode' );
-
-?>
